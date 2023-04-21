84bf397e6e13883b1bbdd527faa1306fce036999
Kagheni <Kaghenimbale@gmail.com>
2023-04-21 11:38:05 +0200

    Add navbar folder to the component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Details from './components/Details/Details';
-import Navbar from './components/Details/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
